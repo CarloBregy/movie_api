18903d9d4bc2eba7921b0efda68399d07c372b38
CB <carlo_bregy@outlook.com>
2025-09-10 11:10:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ app.post('/users',  [
         Users
           .create({
             Username: req.body.Username,
-            Password: hashedPassword,
+            Password: req.body.Password,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
